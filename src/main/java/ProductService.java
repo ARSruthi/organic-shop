99a4f9b1b7c739536ef9d3a03e7ac1de4f3740ae
ARSruthi <sruthianbu96@gmail.com>
2026-03-23 17:43:59 +0530

    Create ProductService.java

---
+++ src/main/java/ProductService.java
@@ -0,0 +1,25 @@
+package com.example.organicshop.service;
+
+import com.example.organicshop.model.Product;
+import com.example.organicshop.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    public Product addProduct(Product product) {
+        return repository.save(product);
+    }
+}
